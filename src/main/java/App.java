6452ad06d984a824e8434bb31b0e9ae20b2954f3
SHYLA <sheilabernicew@gmail.com>
2019-05-27 09:27:07 +0300

    created a clientupdate  file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,147 @@
-import java.util.Map;
-import java.util.HashMap;
 import static spark.Spark.*;
+import spark.template.velocity.VelocityTemplateEngine;
+import spark.ModelAndView;
+import spark.TemplateEngine;
+
+import static spark.debug.DebugScreen.enableDebugScreen;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.eclipse.jetty.http.MetaData.Request;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
-    }
 
-get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+        setPort(port);
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("stylists", Stylist.allStylist());
             model.put("template", "templates/index.vtl");
-            return new VelocityTemplateEngine().render(
-                    new ModelAndView(model, layout)
-            );
-        });
-    }
+            return new ModelAndView(model, layout);
+        }, new VelocityEngine());
 
+    get("/clients/new",(request, response)->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.allStylist());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityEngine());
+
+    post("/stylist", (request, response)->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      String style = request.queryParams("style");
+      Stylist newStylist = new Stylist(name, style);
+      newStylist.save();
+      model.put("template", "templates/style-success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    spark.template
+    
+    get("/stylist", (reqeust, response)->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.allStylist());
+      model.put("template", "templates/stylist.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylist/:id", (request, response)->{
+      Map<String, Object> model= new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+      model.put("clients", Client.all());
+      model.put("stylist", stylist);
+      model.put("template", "templates/viewStylist.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    //Post method for deleting client
+    post("/client/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      Client client = Client.find(Integer.parseInt(request.params(":id")));
+      client.deleteClient();
+      model.put("stylist", client);
+      model.put("template", "templates/viewClients.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    //Post Method to delete stylist
+    post("/stylist/:id", (request, response) -> {
+        Map<String, Object> model = new HashMap<>();
+        Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+        stylist.delete();
+        model.put("stylist", stylist);
+        model.put("template", "templates/stylist.vtl");
+        return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+    post("/client/:id", (request, response)->{
+      Map<String, Object> model= new HashMap<String, Object>();
+      Client client = Client.find(Integer.parseInt(request.params(":id")));
+      model.put("stylist", client);
+      model.put("template", "templates/viewClients.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/client", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.find(Integer.parseInt(request.queryParams(":id")));
+      String clientName = request.queryParams("clientName");
+      String clientStyle = request.queryParams("clientStyle");
+      Client newClient = new Client(clientName, clientStyle, stylist.getId());
+      newClient.save();
+      model.put("stylist", stylist);
+      model.put("template", "templates/client-success.vtl");
+      return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+    get("/about", (request, response)->{
+      Map<String, Object> model= new HashMap<String, Object>();
+      model.put("clients", Client.all());
+      model.put("template", "templates/AboutUs.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/stylist/:id/viewClients", (request, response)->{
+        Map<String, Object> model= new HashMap<String, Object>();
+        Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+        Client client = Client.find(Integer.parseInt(request.params(":id")));
+        model.put("clients", stylist.getClients());
+        model.put("client", client);
+        model.put("stylist", stylist);
+        model.put("template", "templates/viewClients.vtl");
+        return new ModelAndView(model, layout);
+      }, new TemplateEngine());
+
+      post("/client/:client_id/update", (request, response) -> {
+        Map<String, Object> model = new HashMap<>();
+        Client client = Client.find(Integer.parseInt(request.params(":client_id")));
+        String clientName = request.queryParams("clientName");
+        String clientStyle = request.queryParams("clientStyle");
+        client.update(clientName, clientStyle);
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, layout);
+    }, new VelocityEngine());
+
+    get("/client/:client_id/update", (request, response) -> {
+        Map<String, Object> model = new HashMap<>();
+        Client client = Client.find(Integer.parseInt(request.params(":client_id")));
+        model.put("client", client);
+        model.put("template", "templates/updateClient.vtl");
+        return new ModelAndView(model, layout);
+    }, new VelocityEngine());
+  }
+}
--- src/test/java/clientTest.java
+++ src/test/java/clientTest.java
@@ -1,113 +1,38 @@
-import org.sql2o.*;
-import org.junit.*;
-import static org.junit.Assert.*;
-import java.time.LocalDateTime;
+import static  org.junit.Assert.*;
+import  org.junit.*;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
-class ClientTest {
-    Client newClient, newClient2;
 
-    @BeforeEach
+public class ClientTest {
+    @Before
     public void setUp() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", "kosgei", "12345678");
-        newClient = new Client("jane","doe","0723320981","janedoe@gmail.com","Kenya","Nairobi",1);
-        newClient2 = new Client("john","doe","0723320923","johndoe@gmail.com","Kenya","Mombasa",1);
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/my_salon_test", "fatah", "6674fatah");
     }
-    @AfterEach
-    public void tearDown() {
-        try (Connection con = DB.sql2o.open()) {
+    @After
+    public void tearDown(){
+        try(Connection con = DB.sql2o.open()){
             String sql = "DELETE FROM clients *;";
             con.createQuery(sql).executeUpdate();
         }
     }
     @Test
-    public void newClient_instantiatesCorrectly() {
-        assertTrue(newClient instanceof Client);
+    public void clientReturnsAnInstanceOfObject_object(){
+        Client client = new Client("hll","hjjn",1);
+        assertTrue(client instanceof Object);
+    }
+    @Test
+    public void getNameOfClient_String(){
+        Client client = new Client("hll","hjjn",1);
+        assertEquals("hll", client.getClientName());
     }
 
     @Test
-    public void newClient_getsFirstName_jane()
-    {
-        assertEquals("jane", newClient.getFirst_name());
+    public void returnsClientsAsInstanceOfClientClass_Client(){
+        Client client = new Client("hll","hjjn",1);
+        Client client2 = new Client("dhkj","dhjhj",1);
+
+        assertTrue(Client.all().contains(client));
+        assertTrue(Client.all().contains(client2));
     }
-
-    @Test
-    public void newClient_getsLastName_doe()
-    {
-        assertEquals("doe", newClient.getLast_name());
-    }
-    @Test
-    public void newClient_getsPhone_0723320981()
-    {
-        assertEquals("0723320981",newClient.getPhone());
-    }
-
-    @Test
-    public void newClient_getsEmail_janedoe()
-    {
-        assertEquals("janedoe@gmail.com", newClient.getEmail());
-    }
-
-    @Test
-    public void newClient_getsCountry_Kenya()
-    {
-        assertEquals("Kenya", newClient.getCountry());
-    }
-
-    @Test
-    public void newClient_getsCounty_Nairobi()
-    {
-        assertEquals("Nairobi", newClient.getCounty());
-    }
-
-    @Test
-    public void newClient_getsStylistId_1()
-    {
-        assertEquals(1, newClient.getStylistid());
-    }
-
-    @Test
-    public void all_returnsAllInstancesOfClients_true() {
-       newClient.save();
-       newClient2.save();
-
-        assertEquals(true, Client.all().get(0).equals(newClient));
-        assertEquals(true, Client.all().get(1).equals(newClient2));
-    }
-
-    @Test
-    public void save_assignsIdToObject() {
-        newClient.save();
-        Client savedClient = Client.all().get(0);
-        assertEquals(newClient.getId(), savedClient.getId());
-    }
-
-    @Test
-    public void getId_clientInstantiateWithAnID() {
-        newClient.save();
-        assertTrue(newClient.getId() > 0);
-    }
-
-    @Test
-    public void find_returnsClientWithSameId_newClient2() {
-        newClient.save();
-       newClient2.save();
-
-        assertEquals(Client.find(newClient2.getId()), newClient2);
-    }
-
-    @Test
-    public void delete_deletesClient_true() {
-        newClient.save();
-        int newClientId =newClient.getId();
-        newClient.delete();
-        assertEquals(null, Client.find(newClientId));
-    }
-
-    @Test
-    public void update_updatesClient_true() {
-        newClient.save();
-        newClient.update("chris", "sean","0933772341","johndoe@gmail.com", "kenya", "kisumu",1);
-        assertEquals("chris", Client.find(newClient.getId()).getFirst_name());
-    }
-
 }
